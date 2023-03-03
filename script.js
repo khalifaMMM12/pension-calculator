72444a97de379f8d7cebc0cb1331c3e4962239a7
Gentlemanintech <umaidoki42@gmail.com>
2023-03-03 11:08:04 -0800

    modified it

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-const calculateButton = document.querySelector('#calculateBtn');
+// RETIREMENT SUM CALCULATOR
+const calculateButton = document.querySelector('#RS_calculateBtn');
 
 function calculateRetirementSum(e) {
   e.preventDefault();
-  //The currentAge and retirementAge is optional. Unless you want to automate the n.
-  //const currentAge = Number(document.querySelector('#currentAge').value);
-  //const retirementAge = Number(document.querySelector('#retirementAge').value);
-  /*const n = Number(retirementAge - currentAge);
-  You can use the above code of const n to get value of n without inputing anything on the n textfield.
-  */
   const n = Number(document.querySelector('#n').value);
   const PV = Number(document.querySelector('#PV').value);
   const c = Number(document.querySelector('#c').value);
@@ -17,9 +12,34 @@ function calculateRetirementSum(e) {
   const FV = c * ((Math.pow(1 + r / 12, n * 12) - 1) / (r / 12)) * (1 + g) ** n + PV * (1 + r) ** n;
 
   const retirementSumOutput = document.querySelector('#retirementSum');
-  retirementSumOutput.textContent = 'Your Retirement Sum is = ₦' + FV.toLocaleString();
+  retirementSumOutput.textContent = 'Your Retirement Sum is = ₦' + FV.toLocaleString;
 }
 
 calculateButton.addEventListener('click', calculateRetirementSum);
 
-//basically the current age and retirement age are not in the formula it's just there for the sake of karin bayyani.
+
+// CAPITAL RECOVERY CALCULATOR
+const calculateBtn = document.querySelector('#CR_calculateBtn');
+
+function calculateCapitalRecovery(e) {
+  e.preventDefault();
+  const pw = Number(document.querySelector('#pw').value);
+  const l = Number(document.querySelector('#L').value);
+  const n12 = Number(document.querySelector('#n12').value);
+  //const n = Number(document.querySelector('#no').value);
+  const pv = Number(document.querySelector('#pv').value);
+  const r = Number(document.querySelector('#ro').value) / 100;
+
+  let C = (pw * ((1 + r/12)**(n*12) - 1) / (r/12)) - (pv * (1 + r/12)**(n12)) + (l * ((1 + r/12)**(n*12) - 1)) / (r/12 * (1 + r/12)**(n*12))
+
+  const CROutPut = document.querySelector('#CapitalRecovery');
+  CROutPut.textContent = 'Your Capital Recovery is = ₦' + C.toLocaleString();
+  console.log(C)
+}
+
+calculateBtn.addEventListener('click', calculateCapitalRecovery);
+
+//Just figure out the formula and make it right the code problem is Id's you can't use the same id for two boxes
+//I've changed the r id. It'll always take the first Id value which is 0 and compute. That's the reason for the NaN.
+
+

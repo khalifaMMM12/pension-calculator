4fe4c6b90f0085a82353bacadf68323d75ee06ad
Gentlemanintech <108980260+Gentlemanintech@users.noreply.github.com>
2023-03-11 13:37:30 -0800

    Deleted

--- chart.js
+++
@@ -1,4 +0,0 @@
-const RS_Btn = document.getElementById('RS_calculateBtn');
-const currentPV = document.getElementById('PV');
-const retirementSum = document.getElementById('retirementSumValue');
-const chartCanvas = document.getElementById('myChart');

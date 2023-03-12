678dfefed8f6ea9e78d555a673c8e11d15143799
Gentlemanintech <umaidoki42@gmail.com>
2023-03-11 22:21:01 -0800

    Modified

--- chart.js
+++ chart.js
@@ -1,38 +1,4 @@
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const chartData = {
-      labels: ['Property 1', 'Property 2'],
-      datasets: [{
-        label: 'Values',
-        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
-        borderWidth: 1,
-        data: [10, 20]
-      }]
-    };
-    const chartOptions = {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    };
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: chartData,
-      options: chartOptions
-    });
-
-    // Update chart data
-    function updateChartData() {
-      const property1Value = Number(document.getElementById('currentRSA').value);
-      const property2Value = Number(document.getElementById('retirementSum').value);
-      myChart.data.datasets[0].data[0] = property1Value;
-      myChart.data.datasets[0].data[1] = property2Value;
-      myChart.update();
-    }
-
-    // Add event listeners to input fields
-    document.getElementById('currentRSA').addEventListener('input', updateChartData);
-    document.getElementById('').addEventListener('input', updateChartData);
+const RS_Btn = document.getElementById('RS_calculateBtn');
+const currentPV = document.getElementById('PV');
+const retirementSum = document.getElementById('retirementSumValue');
+const chartCanvas = document.getElementById('myChart');
--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-// RETIREMENT SUM CALCULATOR
-const calculateButton = document.querySelector('#RS_calculateBtn');
-
-function calculateRetirementSum(e) {
-  e.preventDefault();
+  // RETIREMENT SUM CALCULATOR
+  const calculateButton = document.querySelector('#RS_calculateBtn');
+  
+  function calculateRetirementSum(e) {
+    e.preventDefault();
   const n = Number(document.querySelector('#n').value);
   const PV = Number(document.querySelector('#PV').value);
   const c = Number(document.querySelector('#c').value);
   const g = Number(document.querySelector('#g').value) / 100;
   const r = Number(document.querySelector('#r').value) / 100;
-
+  
   const FV = c * ((Math.pow(1 + r / 12, n * 12) - 1) / (r / 12)) * (1 + g) ** n + PV * (1 + r) ** n;
+  
+  const retirementSumOutput = document.querySelector('#retirementSumValue');
+  retirementSumOutput.textContent = 'Your Retirement Sum is = ₦' + FV.toLocaleString();
+  
 
-  const retirementSumOutput = document.querySelector('#retirementSum');
-  retirementSumOutput.textContent = 'Your Retirement Sum is = ₦' + FV.toLocaleString;
-}
+  //Chart Workings here
+  let myChart;
+
+  const data = {
+    labels: ['Current RSA balance', 'Retirement Sum'],
+    datasets: [{
+      label: 'Amount in Naira',
+      data: [PV, FV],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+  
+  const chartCtx = chartCanvas.getContext('2d');
+  
+  if (myChart) {
+    myChart.destroy();
+  }
+  
+  myChart = new Chart(chartCtx, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            beginAtZero: true,
+            suggestedMax: 10000000
+          }
+        }
+      }
+    }
+    
+  });
+  
+
+}//end of calculate retirement sum function
 
 calculateButton.addEventListener('click', calculateRetirementSum);
-
 
 // CAPITAL RECOVERY CALCULATOR
 const calculateBtn = document.querySelector('#CR_calculateBtn');
@@ -25,21 +69,17 @@ function calculateCapitalRecovery(e) {
   e.preventDefault();
   const pw = Number(document.querySelector('#pw').value);
   const l = Number(document.querySelector('#L').value);
-  const n12 = Number(document.querySelector('#n12').value);
-  //const n = Number(document.querySelector('#no').value);
+  const n = Number(document.querySelector('#c_n').value) * 12;
   const pv = Number(document.querySelector('#pv').value);
-  const r = Number(document.querySelector('#ro').value) / 100;
-
-  let C = (pw * ((1 + r/12)**(n*12) - 1) / (r/12)) - (pv * (1 + r/12)**(n12)) + (l * ((1 + r/12)**(n*12) - 1)) / (r/12 * (1 + r/12)**(n*12))
-
+  const r = Number(document.querySelector('#c_r').value) / 100;
+  
+  const C = pw * (Math.pow((1 + (r / 12)), (21 * 12)) - 1) - (pv * (r / 12) * ((1 + (r / 12)) ** n))
+  + ((l * (r / 12)) / (((1 + (r / 12)) ** n) - 1));
+  
+  
+  
   const CROutPut = document.querySelector('#CapitalRecovery');
   CROutPut.textContent = 'Your Capital Recovery is = ₦' + C.toLocaleString();
- ;
 }
 
 calculateBtn.addEventListener('click', calculateCapitalRecovery);
-
-//Just figure out the formula and make it right the code problem is Id's you can't use the same id for two boxes
-//I've changed the r id. It'll always take the first Id value which is 0 and compute. That's the reason for the NaN.
-
-

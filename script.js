ecb3d5def99a8dbb6b637dc29f8c493a38aaa9b2
khalifaMMM12 <103419536+khalifaMMM12@users.noreply.github.com>
2023-03-28 22:51:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
   
   function calculateRetirementSum(e) {
     e.preventDefault();
-  const n = Number(document.querySelector('#n').value);
-  const PV = Number(document.querySelector('#PV').value);
-  const c = Number(document.querySelector('#c').value);
-  const g = Number(document.querySelector('#g').value) / 100;
-  const r = Number(document.querySelector('#r').value) / 100;
+  const n = Number(document.querySelector('#workYears').value);
+  const PV = Number(document.querySelector('#RSA_balance').value);
+  const c = Number(document.querySelector('#M_C').value);
+  const g = Number(document.querySelector('#A_C').value) / 100;
+  const r = Number(document.querySelector('#rate').value) / 100;
   
   const FV = c * ((Math.pow(1 + r / 12, n * 12) - 1) / (r / 12)) * (1 + g) ** n + PV * (1 + r) ** n;
   
@@ -72,11 +72,13 @@ let CR_Chart = null
 function calculateCapitalRecovery(e) {
   e.preventDefault();
   const pw = Number(document.querySelector('#pw').value);
-  const l = Number(document.querySelector('#L').value);
+  const l = Number(document.querySelector('#Lump').value);
   const n = Number(document.querySelector('#c_n').value) * 12;
-  const pv = Number(document.querySelector('#pv').value);
-  const r = Number(document.querySelector('#c_r').value) / 100;
-  const m = Number(document.querySelector('#M_C').value);
+  const Qn = Number(document.querySelector('#c_n').value) * 4;
+  const Yn = Number(document.querySelector('#c_n').value) * 1;
+  const pv = Number(document.querySelector('#RSA_balanceCR').value);
+  const r = Number(document.querySelector('#RoR').value) / 100;
+  const m = Number(document.querySelector('#MC_CR').value);
   
   const CR =(-1 * (pw * (1 - (1 + r / n) ** (-21 * n)) - (pv * (r / n) * (1 + r / n) ** (n)) + (l * r / n)) / ((1 + r / n) ** n - 1)); 
 
@@ -96,10 +98,10 @@ function calculateCapitalRecovery(e) {
   function calculatemonthlyCR(e){
     e.preventDefault();
 
-    const C_R =((pw * (1 - (1 + r / 12) ** (-21 * 12)) - (pv * (r / 12) * (1 + r / 12) ** (n)) + (l * r / 12)) / ((1 + r / 12) ** n - 1)); 
+    const C_R =((pw * (1 - (1 + r / 12) ** (-21 * 12)) - (pv * (r / 12) * (1 + r / 12) ** (n)) + (l * r / 12)) / ((1 + r / 12) ** n- 1)); 
     
     const monthlyCRoutput = document.querySelector('#MonthlyCR');
-    monthlyCRoutput.textContent = 'Your Monthly Capital Recovery is = ₦' + C_R.toLocaleString();
+    monthlyCRoutput.textContent = `Your Monthly Capital Recovery was ${m.toLocaleString()} and Now = ₦ ${C_R.toLocaleString()}`;
 }
 monthlyCR.addEventListener('click', calculatemonthlyCR);
 
@@ -110,7 +112,7 @@ const QuarterlyCR = document.querySelector('#quarterlyCR_BTN');
 function  calculatequarterlyCR(e){
   e.preventDefault();
 
-  const Quarterly_CR =(-1 *(pw * (1 - (1 + r / 4) ** (-21 * 4)) - (pv * (r / 4) * (1 + r / 4) ** (n)) + (l * r / 4)) / ((1 + r / 4) ** n - 1)); 
+  const Quarterly_CR =((pw * (1 - (1 + r / 12) ** (-21 * 12)) - (pv * (r / 12) * (1 + r / 12) ** (Qn)) + (l * r / 12)) / ((1 + r / 12) ** Qn- 1)); 
 
   const QuarterlyCRoutput = document.querySelector('#quarterlyCR');
   QuarterlyCRoutput.textContent = 'Your Quarterly Capital Recovery is = ₦' + Quarterly_CR.toLocaleString();
@@ -123,7 +125,7 @@ const yearlyCR = document.querySelector('#yearlyCR_BTN');
 function calculateyearlyCR(e){
   e.preventDefault();
 
-  const yearly_CR = (-1 *(pw * (1 - (1 + r / 1) ** (-21 * 1)) - (pv * (r / 1) * (1 + r / 1) ** (n)) + (l * r / 1)) / ((1 + r / 1) ** n - 1)); 
+  const yearly_CR = ((pw * (1 - (1 + r / 12) ** (-21 * 12)) - (pv * (r / 12) * (1 + r / 12) ** (n*1)) + (l * r / 12)) / ((1 + r / 12) ** Yn- 1)); 
 
   const yearlyCRoutput = document.querySelector('#yearlyCR');
   yearlyCRoutput.textContent = 'Your Yearly Capital Recovery is = ₦' + yearly_CR.toLocaleString();
@@ -133,21 +135,16 @@ yearlyCR.addEventListener('click', calculateyearlyCR);
 
 // DROP DOWN OPTIONS END
 
-
-
-  // const CROutPut = document.querySelector('#MonthlyCR');
-  // CROutPut.textContent = 'Your Monthly Capital Recovery is = ₦' + C_R.toLocaleString();
-
   // const FGoutput = document.querySelector("#FundingGap");
   // FGoutput.textContent = 'Your Funding Gap is =  ₦' + F_G.toLocaleString();
   
   const chartCanvas = document.getElementById('CR_Chart');
 
   const data = {
-    labels: ['Current Monthly Contribution', 'Funding Gap', 'New Monthly Contribution'],
+    labels: ['Current Monthly Contribution', 'New Monthly Contribution'],
     datasets: [{
       label: 'Amount in Naira',
-      data: [m, C_R, F_G],
+      data: [m, CR],
       backgroundColor: [
         'rgba(147, 236, 147, 0.8)',
         'rgba(255, 99, 132, 0.5)',
